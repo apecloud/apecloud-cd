1d087a7db9f25fec6665a57411d61d1d6f106a18
huangzhangshu <huangzhangshu@apecloud.com>
2023-07-11 22:01:40 +0800

    chore: filter upload chart to jihulab

--- .github/utils/release_gitlab.sh
+++ .github/utils/release_gitlab.sh
@@ -195,6 +195,9 @@ release_helm() {
     ASSET_PATHS=()
     if [[ -d "$ASSET_PATH" ]]; then
         for asset_path in $ASSET_PATH/*; do
+            if [[ -z "$ASSET_NAME" && "$asset_path" != "${ASSET_NAME}-"* ]]; then
+                continue
+            fi
             ASSET_PATHS[${#ASSET_PATHS[@]}]=`basename $asset_path`
         done
     elif [[ -f "$ASSET_PATH" ]]; then
