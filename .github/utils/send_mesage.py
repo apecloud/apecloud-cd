1b1377be203f6b3d35bb85e435ae8545332b398a
huangzhangshu <109708205+JashBook@users.noreply.github.com>
2023-09-17 12:04:52 +0800

    chore: support performance test workflow (#85)
    * chore: performance test
    
    * chore: performance test

--- .github/utils/send_mesage.py
+++ .github/utils/send_mesage.py
@@ -8,11 +8,15 @@ parser = argparse.ArgumentParser(description='Test for argparse')
 parser.add_argument('--title', '-t', help='test title', default="")
 parser.add_argument('--result', '-r', help='test result', default="")
 parser.add_argument('--url', '-u', help='webhook url', default="")
+parser.add_argument('--job-url', '-ju', help='github job url', default="")
+parser.add_argument('--send-type', '-st', help='test type', default="")
 
 args = parser.parse_args()
 title = args.title
 result = args.result
 url = args.url
+job_url = args.job_url
+send_type = args.send_type
 
 
 def send_message(url_v, result_v, title_v):
@@ -112,6 +116,154 @@ def send_message(url_v, result_v, title_v):
     print(res.text)
 
 
+def send_performance_message(url_v, result_v, title_v, job_url_v):
+    print("send performance message")
+    json_results = []
+    json_ret = {
+        "tag": "column_set",
+        "flex_mode": "none",
+        "background_style": "grey",
+        "columns": [
+            {
+                "tag": "column",
+                "width": "weighted",
+                "weight": 1,
+                "vertical_align": "top",
+                "elements": [
+                    {
+                        "tag": "markdown",
+                        "content": "**THREADS**",
+                        "text_align": "center"
+                    }
+                ]
+            },
+            {
+                "tag": "column",
+                "width": "weighted",
+                "weight": 1,
+                "vertical_align": "top",
+                "elements": [
+                    {
+                        "tag": "markdown",
+                        "content": "**QPS**",
+                        "text_align": "center"
+                    }
+                ]
+            },
+            {
+                "tag": "column",
+                "width": "weighted",
+                "weight": 1,
+                "vertical_align": "top",
+                "elements": [
+                    {
+                        "tag": "markdown",
+                        "content": "**TPS**",
+                        "text_align": "center"
+                    }
+                ]
+            },
+            {
+                "tag": "column",
+                "width": "weighted",
+                "weight": 1,
+                "vertical_align": "top",
+                "elements": [
+                    {
+                        "tag": "markdown",
+                        "content": "**Latency(ms)**",
+                        "text_align": "center"
+                    }
+                ]
+            }
+        ]
+    }
+    json_results.append(json_ret)
+    if result_v:
+        result_array = result_v.split("##")
+        for results in result_array:
+            if results:
+                ret = results.split("#")
+                json_ret = {
+                    "tag": "column_set",
+                    "flex_mode": "none",
+                    "background_style": "default",
+                    "columns": [
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "markdown",
+                                    "content": "<a href='" + job_url_v + "'>" + ret[0] + "</a>",
+                                    "text_align": "center"
+                                }
+                            ]
+                        },
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "markdown",
+                                    "content": "<font color='green'>" + ret[1] + "</font>",
+                                    "text_align": "center"
+                                }
+                            ]
+                        },
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "markdown",
+                                    "content": "<font color='green'>" + ret[2] + "</font>",
+                                    "text_align": "center"
+                                }
+                            ]
+                        },
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "markdown",
+                                    "content": "<font color='green'>" + ret[3] + "</font>",
+                                    "text_align": "center"
+                                }
+                            ]
+                        }
+                    ],
+                }
+                json_results.append(json_ret)
+
+    card = json.dumps({
+        "header": {
+            "template": "blue",
+            "title": {
+                "tag": "plain_text",
+                "content": title_v
+            }
+        },
+        "elements": json_results
+    })
+    body = json.dumps({"msg_type": "interactive", "card": card})
+    headers = {"Content-Type": "application/json"}
+    res = requests.post(url=url_v, data=body, headers=headers)
+    print(res.text)
+
+
 if __name__ == '__main__':
-    send_message(url, result, title)
+    if send_type == "performance":
+        send_performance_message(url, result, title, job_url)
+    else:
+        send_message(url, result, title)
     
