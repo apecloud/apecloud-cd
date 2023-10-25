102286ad38e44d1fd68fce01b66fe71aeddd69b9
huangzhangshu <109708205+JashBook@users.noreply.github.com>
2023-10-25 17:25:25 +0800

    chore: only package specify chart dir (#157)

--- .github/utils/helm_package.sh
+++ .github/utils/helm_package.sh
@@ -11,130 +11,12 @@ cat << EOF
 Usage: $(basename "$0") <options>
 
     -h, --help               Display help
-    -v, --version            The chart-releaser version to use (default: $DEFAULT_CHART_RELEASER_VERSION)"
     -d, --charts-dir         The charts directory (default: charts)
-    -o, --owner              The repo owner
-    -r, --repo               The repo name
-    -n, --install-dir        The Path to install the cr tool
     -rv, --release-version   The release version of helm charts
     -t, --tool               The tool of helm package (helm,cr default: $DEFAULT_TOOL)
     -ic, --install-cr        Install chart releaser (default: $DEFAULT_INSTALL_CR)
+    -sc, --specify-chart     Only package the specify sub dir chart
 EOF
-}
-
-main() {
-    local version="$DEFAULT_CHART_RELEASER_VERSION"
-    local charts_dir=charts
-    local owner=
-    local repo=
-    local install_dir=
-    local release_version=""
-    local tool=$DEFAULT_TOOL
-    local install_cr=$DEFAULT_INSTALL_CR
-
-    parse_command_line "$@"
-
-    : "${CR_TOKEN:?Environment variable CR_TOKEN must be set}"
-
-    local repo_root
-    repo_root=$(git rev-parse --show-toplevel)
-    pushd "$repo_root" > /dev/null
-
-    echo "Lookup charts..."
-    local changed_charts=()
-    readarray -t changed_charts <<< "$(lookup_changed_charts)"
-
-    if [[ -n "${changed_charts[*]}" || "$install_cr" == "true" ]]; then
-        install_chart_releaser
-
-        rm -rf .cr-release-packages
-        mkdir -p .cr-release-packages
-
-        rm -rf .cr-index
-        mkdir -p .cr-index
-
-        for chart in "${changed_charts[@]}"; do
-            if [[ -d "$chart" ]]; then
-                package_chart "$chart"
-            else
-                echo "Chart '$chart' no longer exists in repo. Skipping it..."
-            fi
-        done
-    else
-        echo "Nothing to do. No chart changes detected."
-    fi
-
-    popd > /dev/null
-}
-
-parse_command_line() {
-    while :; do
-        case "${1:-}" in
-            -h|--help)
-                show_help
-                exit
-                ;;
-            -v|--version)
-                if [[ -n "${2:-}" ]]; then
-                    version="$2"
-                    shift
-                fi
-                ;;
-            -d|--charts-dir)
-                if [[ -n "${2:-}" ]]; then
-                    charts_dir="$2"
-                    shift
-                fi
-                ;;
-            -o|--owner)
-                if [[ -n "${2:-}" ]]; then
-                    owner="$2"
-                    shift
-                fi
-                ;;
-            -r|--repo)
-                if [[ -n "${2:-}" ]]; then
-                    repo="$2"
-                    shift
-                fi
-                ;;
-            -n|--install-dir)
-                if [[ -n "${2:-}" ]]; then
-                    install_dir="$2"
-                    shift
-                fi
-                ;;
-            -rv|--release-version)
-                if [[ -n "${2:-}" ]]; then
-                    release_version="$2"
-                    shift
-                fi
-                ;;
-            -t|--tool)
-                if [[ -n "${2:-}" ]]; then
-                    tool="$2"
-                    shift
-                fi
-                ;;
-            -ic|--install-cr)
-                if [[ -n "${2:-}" ]]; then
-                    install_cr="$2"
-                    shift
-                fi
-                ;;
-            *)
-                break
-                ;;
-        esac
-
-        shift
-    done
-
-    if [[ -z "$install_dir" ]]; then
-        local arch
-        arch=$(uname -m)
-        install_dir="$RUNNER_TOOL_CACHE/cr/$version/$arch"
-    fi
 }
 
 install_chart_releaser() {
@@ -170,7 +52,11 @@ filter_charts() {
 
 lookup_changed_charts() {
     local changed_files
-    changed_files=$(ls -1 "$charts_dir")
+    if [[ -n "$SPECIFY_CHART" ]]; then
+        changed_files="$SPECIFY_CHART"
+    else
+        changed_files=$(ls -1 "$charts_dir")
+    fi
     echo "$changed_files" | filter_charts
 }
 
@@ -204,6 +90,102 @@ package_chart() {
             break
         ;;
     esac
+}
+
+parse_command_line() {
+    while :; do
+        case "${1:-}" in
+            -h|--help)
+                show_help
+                exit
+            ;;
+            -d|--charts-dir)
+                if [[ -n "${2:-}" ]]; then
+                    charts_dir="$2"
+                    shift
+                fi
+            ;;
+            -rv|--release-version)
+                if [[ -n "${2:-}" ]]; then
+                    release_version="$2"
+                    shift
+                fi
+            ;;
+            -t|--tool)
+                if [[ -n "${2:-}" ]]; then
+                    tool="$2"
+                    shift
+                fi
+            ;;
+            -ic|--install-cr)
+                if [[ -n "${2:-}" ]]; then
+                    install_cr="$2"
+                    shift
+                fi
+            ;;
+            -sc, --specify-chart)
+                if [[ -n "${2:-}" ]]; then
+                    SPECIFY_CHART="$2"
+                    shift
+                fi
+            ;;
+            *)
+                break
+            ;;
+        esac
+
+        shift
+    done
+
+    if [[ -z "$install_dir" ]]; then
+        local arch
+        arch=$(uname -m)
+        install_dir="$RUNNER_TOOL_CACHE/cr/$version/$arch"
+    fi
+}
+
+main() {
+    local version="$DEFAULT_CHART_RELEASER_VERSION"
+    local charts_dir=charts
+    local install_dir=""
+    local release_version=""
+    local tool=$DEFAULT_TOOL
+    local install_cr=$DEFAULT_INSTALL_CR
+    local SPECIFY_CHART=""
+
+    parse_command_line "$@"
+
+    : "${CR_TOKEN:?Environment variable CR_TOKEN must be set}"
+
+    local repo_root
+    repo_root=$(git rev-parse --show-toplevel)
+    pushd "$repo_root" > /dev/null
+
+    echo "Lookup charts..."
+    local changed_charts=()
+    readarray -t changed_charts <<< "$(lookup_changed_charts)"
+
+    if [[ -n "${changed_charts[*]}" || "$install_cr" == "true" ]]; then
+        install_chart_releaser
+
+        rm -rf .cr-release-packages
+        mkdir -p .cr-release-packages
+
+        rm -rf .cr-index
+        mkdir -p .cr-index
+
+        for chart in "${changed_charts[@]}"; do
+            if [[ -d "$chart" ]]; then
+                package_chart "$chart"
+            else
+                echo "Chart '$chart' no longer exists in repo. Skipping it..."
+            fi
+        done
+    else
+        echo "Nothing to do. No chart changes detected."
+    fi
+
+    popd > /dev/null
 }
 
 main "$@"
