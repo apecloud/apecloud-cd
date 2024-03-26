dd67c509bb4aeb3351fe85b5a031a8c56d4db884
huangzhangshu <huangzhangshu@apecloud.com>
2024-03-26 11:15:40 +0800

    chore: skip delete kb 0.9.0-alpha.2

--- .github/utils/utils.sh
+++ .github/utils/utils.sh
@@ -372,7 +372,7 @@ get_delete_release() {
     for tag in $( echo "$release_list" ) ;do
         delete_flag=0
 
-        if [[ "$tag" == *"0.6.0-beta.11"* ]]; then
+        if [[ "$tag" == *"0.6.0-beta.11"* || "$tag" == *"0.9.0-alpha.2"*  ]]; then
             TAG_NAME=""
             continue
         fi
