e69ff8aef9f4684a0bc743119343c63fd80e7699
huangzhangshu <109708205+JashBook@users.noreply.github.com>
2024-12-03 17:57:48 +0800

    chore: refine update chart values workflow (#500)
    * chore: refine update chart values workflow
    
    * chore: change release-values.yaml

--- .github/utils/update_chart_values.sh
+++ .github/utils/update_chart_values.sh
@@ -7,13 +7,9 @@ cat << EOF
 Usage: $(basename "$0") <options>
 
     -h, --help                Display help
-    -t, --type                Update chart type
-                                1) commit
-                                2) release
-    -cd, --chart-dir          Specify all charts dir (e.g. addons|addons-cluster)
-    -bv, --base-values        Base values file path for update (e.g. commit-values.yaml or release-values.yaml)
-    -uc, --update-chart       Specify update charts dir (e.g. addons/mysql|addons/postgresql)
-    -rn, --ref-name           Ref name for update chart (commit id or branch name)
+    -cd, --chart-dir          Update charts dir (e.g. addons|addons-cluster)
+    -bv, --base-values        Base values file path for update
+    -bn, --branch-name        Branch name for update chart 
 EOF
 }
 
@@ -23,12 +19,6 @@ parse_command_line() {
             -h|--help)
                 show_help
                 exit
-            ;;
-            -t|--type)
-                if [[ -n "${2:-}" ]]; then
-                    TYPE="$2"
-                    shift
-                fi
             ;;
             -bv|--base-values)
                 if [[ -n "${2:-}" ]]; then
@@ -42,15 +32,9 @@ parse_command_line() {
                     shift
                 fi
             ;;
-            -uc|--update-chart)
+            -bn|--branch-name)
                 if [[ -n "${2:-}" ]]; then
-                    UPDATE_DIR="$2"
-                    shift
-                fi
-            ;;
-            -rn|--ref-name)
-                if [[ -n "${2:-}" ]]; then
-                    REF_NAME="$2"
+                    BRANCH_NAME="$2"
                     shift
                 fi
             ;;
@@ -65,38 +49,32 @@ parse_command_line() {
 
 update_values_file() {
     update_values_file=${1:-""}
+    update_commit_id=${2:-""}
+    update_commit_time=${3:-""}
     if [[ -z "${update_values_file}" ]]; then
         return
     fi
     echo "Updating values ${TYPE} info $update_values_file"
-    case $TYPE in
-        commit)
-            if [[ -z "${REF_NAME}" ]]; then
-                REF_NAME="$(git rev-parse HEAD)"
-            fi
 
-            if [[ "$UNAME" == "Darwin" ]]; then
-                sed -i '' "s/^  commitTime:.*/  commitTime: \"${CURRENT_TIME}\"/" "${update_values_file}"
-                sed -i '' "s/^  commitId:.*/  commitId: \"${REF_NAME}\"/" "${update_values_file}"
-            else
-                sed -i "s/^  commitTime:.*/  commitTime: \"${CURRENT_TIME}\"/" "${update_values_file}"
-                sed -i "s/^  commitId:.*/  commitId: \"${REF_NAME}\"/" "${update_values_file}"
-            fi
-        ;;
-        release)
-            if [[ -z "${REF_NAME}" ]]; then
-                REF_NAME="$(git rev-parse --abbrev-ref HEAD)"
-            fi
+    if [[ -z "${CURRENT_TIME}" ]]; then
+        CURRENT_TIME="$(date '+%Y-%m-%d %H:%M:%S %z')"
+    fi
 
-            if [[ "$UNAME" == "Darwin" ]]; then
-                sed -i '' "s/^  releaseTime:.*/  releaseTime: \"${CURRENT_TIME}\"/" "${update_values_file}"
-                sed -i '' "s/^  releaseBranch:.*/  releaseBranch: \"${REF_NAME}\"/" "${update_values_file}"
-            else
-                sed -i "s/^  releaseTime:.*/  releaseTime: \"${CURRENT_TIME}\"/" "${update_values_file}"
-                sed -i "s/^  releaseBranch:.*/  releaseBranch: \"${REF_NAME}\"/" "${update_values_file}"
-            fi
-        ;;
-    esac
+    if [[ -z "${BRANCH_NAME}" ]]; then
+        BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
+    fi
+
+    if [[ "$UNAME" == "Darwin" ]]; then
+        sed -i '' "s/^  releaseTime:.*/  releaseTime: \"${CURRENT_TIME}\"/" "${update_values_file}"
+        sed -i '' "s/^  releaseBranch:.*/  releaseBranch: \"${BRANCH_NAME}\"/" "${update_values_file}"
+        sed -i '' "s/^  commitTime:.*/  commitTime: \"${update_commit_time}\"/" "${update_values_file}"
+        sed -i '' "s/^  commitId:.*/  commitId: \"${update_commit_id}\"/" "${update_values_file}"
+    else
+        sed -i "s/^  releaseTime:.*/  releaseTime: \"${CURRENT_TIME}\"/" "${update_values_file}"
+        sed -i "s/^  releaseBranch:.*/  releaseBranch: \"${BRANCH_NAME}\"/" "${update_values_file}"
+        sed -i "s/^  commitTime:.*/  commitTime: \"${update_commit_time}\"/" "${update_values_file}"
+        sed -i "s/^  commitId:.*/  commitId: \"${update_commit_id}\"/" "${update_values_file}"
+    fi
 }
 
 append_values_file() {
@@ -109,21 +87,15 @@ append_values_file() {
 
 update_chart_values_file() {
     update_chart_values_file=${1:-""}
-    update_flag=""
-    case $TYPE in
-        commit)
-            update_flag=$(grep 'commitInfo:' ${update_chart_values_file} || true)
-        ;;
-        release)
-            update_flag=$(grep 'releaseInfo:' ${update_chart_values_file} || true)
-        ;;
-    esac
+    update_chart_commit_id=${2:-""}
+    update_chart_commit_time=${3:-""}
 
-    if [[ -n "${update_flag}" ]]; then
-        update_values_file "${update_chart_values_file}"
-    else
+    update_flag=$(grep 'releaseInfo:' ${update_chart_values_file} || true)
+
+    if [[ -z "${update_flag}" ]]; then
         append_values_file "${update_chart_values_file}"
     fi
+    update_values_file "${update_chart_values_file}" "${update_chart_commit_id}" "${update_chart_commit_time}"
 }
 
 update_chart_values() {
@@ -136,41 +108,34 @@ update_chart_values() {
     for update_chart in $(echo "${UPDATE_CHART_DIR}" | sed 's/|/ /g' ); do
         echo "find ${update_chart} values.yaml"
         find "$update_chart" -type f -name 'values.yaml' | while read -r values_file; do
-            update_chart_values_file "${values_file}"
+            update_chart_dir_tmp="${values_file%/*}"
+            chart_log_info=$(git log -n 1 --pretty="format:%H %ad" --date="iso8601" -- "${update_chart_dir_tmp}")
+            IFS=' ' read -r commit_id commit_time <<< "$chart_log_info"
+            update_chart_values_file "${values_file}" "${commit_id}" "${commit_time}"
         done
     done
 
-    echo "update all charts values file ${TYPE} info done"
+    echo "update all charts values file release info done"
 }
 
 main() {
-    local TYPE=""
     local BASE_VALUES=""
     local CHART_DIR=""
-    local UPDATE_DIR=""
-    local REF_NAME=""
-    local CURRENT_TIME="$(date -u '+%Y-%m-%d %H:%M:%S')"
+    local BRANCH_NAME=""
+    local CURRENT_TIME=""
     local UNAME="$(uname -s)"
 
     parse_command_line "$@"
 
-    if [[ -z "${TYPE}" || -z "${BASE_VALUES}" || (-z "${CHART_DIR}" && -z "${UPDATE_DIR}") ]]; then
-        echo "Missing required arguments"
+    if [[ -z "${CHART_DIR}" ]]; then
+        echo "Chart dir is empty."
         return
     fi
-    
-    if [[ "${TYPE}" != "commit" && "${TYPE}" != "release" ]]; then
-        echo "Invalid type ${TYPE}"
-        return
-    fi
-    
 
-    if [[ ! -f "$BASE_VALUES" ]]; then
+    if [[ -z "${BASE_VALUES}" || ! -f "$BASE_VALUES" ]]; then
         echo "$BASE_VALUES file not found."
         return
     fi
-
-    update_values_file "${BASE_VALUES}"
 
     update_chart_values
 }
