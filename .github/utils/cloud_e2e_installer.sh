c4bbff840c5b0fd29f083837033f30d069200ec8
huangzhangshu <109708205+JashBook@users.noreply.github.com>
2025-10-14 12:02:10 +0800

    chore: add cloud_e2e_installer.sh (#633)
    * chore: add cloud_e2e_installer.sh
    
    * chore: echo patch kb-monitor resources

---
+++ .github/utils/cloud_e2e_installer.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+set +e
+set -o nounset
+
+patch_kb_monitor_metrics_collector_resources() {
+    echo "Patch kb-monitor-metrics-collector resources"
+    kubectl patch deployment kb-monitor-metrics-collector -n kb-system --type='json' -p='[
+  {
+    "op": "replace",
+    "path": "/spec/template/spec/containers/0/resources",
+    "value": {
+      "requests": {
+        "memory": "128Mi",
+        "cpu": "100m"
+      },
+      "limits": {
+        "memory": "2Gi",
+        "cpu": "1"
+      }
+    }
+  }
+]'
+}
+
+patch_kb_monitor_resources() {
+    echo "Patch kb-monitor resources"
+    kubectl patch daemonset kb-monitor -n kb-system --type='json' -p='[
+  {
+    "op": "replace",
+    "path": "/spec/template/spec/containers/0/resources",
+    "value": {
+      "requests": {
+        "memory": "128Mi",
+        "cpu": "100m"
+      },
+      "limits": {
+        "memory": "2Gi",
+        "cpu": "2"
+      }
+    }
+  }
+]'
+}
+
+check_kb_monitor_metrics_collector_resources() {
+    kb_monitor_metrics_collector_resources=$(kubectl get deployment kb-monitor-metrics-collector -n kb-system -o json | jq '.spec.template.spec.containers[0].resources')
+    kb_monitor_metrics_collector_request_cpu=$(echo "${kb_monitor_metrics_collector_resources}" | jq -r '.requests.cpu')
+    kb_monitor_metrics_collector_request_memory=$(echo "${kb_monitor_metrics_collector_resources}" | jq -r '.requests.memory')
+    if [[ ! ("${kb_monitor_metrics_collector_request_cpu}" == "100m" || "${kb_monitor_metrics_collector_request_memory}" == "128Mi") ]]; then
+        echo "kb_monitor_metrics_collector_resources:${kb_monitor_metrics_collector_resources}"
+        patch_kb_monitor_metrics_collector_resources
+    fi
+
+}
+
+check_kb_monitor_resources() {
+    kb_monitor_resources=$(kubectl get daemonset kb-monitor -n kb-system -o json | jq '.spec.template.spec.containers[0].resources')
+    kb_monitor_request_cpu=$(echo "${kb_monitor_resources}" | jq -r '.requests.cpu')
+    kb_monitor_request_memory=$(echo "${kb_monitor_resources}" | jq -r '.requests.memory')
+    if [[ ! ("${kb_monitor_request_cpu}" == "100m" || "${kb_monitor_request_memory}" == "128Mi") ]]; then
+        echo "kb_monitor_resources:${kb_monitor_resources}"
+        patch_kb_monitor_resources
+    fi
+}
+
+check_kb_monitor() {
+    patch_times=1
+    patch_flag=0
+    while true; do
+        kb_monitor_metrics_collector_deployment=$(kubectl get deployment -n kb-system | (grep "kb-monitor-metrics-collector" || true ))
+        if [[ -n "${kb_monitor_metrics_collector_deployment}" ]]; then
+            check_kb_monitor_metrics_collector_resources
+            patch_flag=1
+        fi
+
+        kb_monitor_daemonset=$(kubectl get daemonset -n kb-system | (grep "kb-monitor" || true ))
+        if [[ -n "${kb_monitor_daemonset}" ]]; then
+            check_kb_monitor_resources
+            patch_flag=1
+        fi
+
+        if [[ $patch_flag -eq 1 ]]; then
+            patch_times=$(( $patch_times + 1 ))
+            patch_flag=0
+        fi
+
+        if [[ $patch_times -gt 5 ]]; then
+            echo "Patch kb-monitor resources done"
+            break
+        fi
+
+        sleep 5
+    done
+}
+
+check_kb_monitor
