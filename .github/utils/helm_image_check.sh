ed72ee70cb334264f3f3f72f56015108560b603d
huangzhangshu <huangzhangshu@apecloud.com>
2025-12-22 12:08:56 +0800

    chore: ignore check image docker.io/elasticsearch:7.10.2

--- .github/utils/helm_image_check.sh
+++ .github/utils/helm_image_check.sh
@@ -55,7 +55,7 @@ main() {
                 continue
             fi
 
-            if [[ "$repository" == "busybox:busybox"* || "$repository" == "docker.io/library/busybox:busybox"* ]]; then
+            if [[ "$repository" == "busybox:busybox"* || "$repository" == "docker.io/library/busybox:busybox"* || "$repository" == "docker.io/elasticsearch:7.10.2" ]]; then
                 repository=""
                 continue
             fi
